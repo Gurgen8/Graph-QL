c98e09e1f4e0a75f85d0fd89fdb40fbce63f0cc3
GURGEN <84467354+Gurgen8@users.noreply.github.com>
2022-09-28 16:01:34 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <h1>React + Apollo Client</h1>
+      <h1>Apollo Client</h1>
       <Continents />
     </ApolloProvider>
   );
